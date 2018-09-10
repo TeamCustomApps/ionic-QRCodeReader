fd77c54f28605cda8b22a79339452d9a7b0317a8
Lazo07 <noreenlazo07@gmail.com>
2018-09-11 02:23:35 +0800

    Added QR code scanner and text output for QR code value.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+import { hasLifecycleHook } from '@angular/compiler/src/lifecycle_reflector';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  scanData : {};
+  outs:any;
+  options :BarcodeScannerOptions;
+  constructor(public navCtrl: NavController, public navParams: NavParams,private barcodeScanner: BarcodeScanner) {
+  }    
 
-  constructor(public navCtrl: NavController) {
+  scan(){
+    this.options = {
+        prompt : "Scan your barcode "
+    }
+    this.barcodeScanner.scan(this.options).then((barcodeData) => {
 
+        console.log(barcodeData);
+        this.scanData = barcodeData.text;
+    }, (err) => {
+        console.log("Error occured : " + err);
+    });     
   }
-
 }
